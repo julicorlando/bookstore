62f951a758358caedf0c35a07ee0cd83fc525ebb
Júlio Orlando <falecomjulioorlando@outlook.com>
2026-09-09 11:42:15 -0300

    chore: destaca DRF em INSTALLED_APPS

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_extensions",
+    # Django REST Framework - exercício EBAC
     "rest_framework",
     "order",
     "product",
