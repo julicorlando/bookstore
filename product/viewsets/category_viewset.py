fe38b91afb7240223eebd42e5863ce5c7a6175b9
Júlio Orlando <falecomjulioorlando@outlook.com>
2026-09-09 15:43:09 -0300

    feat: aplica paginação no CategoryViewSet

--- product/viewsets/category_viewset.py
+++ product/viewsets/category_viewset.py
@@ -1,11 +1,13 @@
 from rest_framework.viewsets import ModelViewSet
 
+from bookstore.pagination import BookstorePagination
 from product.models import Category
 from product.serializers import CategorySerializer
 
 
 class CategoryViewSet(ModelViewSet):
     serializer_class = CategorySerializer
+    pagination_class = BookstorePagination
 
     def get_queryset(self):
         return Category.objects.all().order_by("-id")
